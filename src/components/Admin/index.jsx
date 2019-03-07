7a387a2c976def6a5b763a9c8eab47536a5e20c2
michaelbrazell <michaelbrazell@gmail.com>
2019-03-07 15:26:10 -0500

    Add advanced user routes to Admin dashboard

--- src/components/Admin/index.jsx
+++ src/components/Admin/index.jsx
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
 import { compose } from 'recompose';
 
 import { withFirebase } from '../Firebase';
 import { withAuthorization } from '../Session';
 import * as ROLES from '../../constants/roles';
+import * as ROUTES from '../../constants/routes';
 
-class AdminPage extends Component {
+const AdminPage = () => (
+  <div>
+    <h1>Admin</h1>
+    <p>The Admin Page is accessible by every signed in admin user.</p>
+
+    <Switch>
+      <Route exact path={ROUTES.ADMIN_DETAILS} component={UserItem} />
+      <Route exact path={ROUTES.ADMIN} component={UserList} />
+    </Switch>
+  </div>
+);
+
+class UserListBase extends Component {
   constructor(props) {
     super(props);
 
@@ -42,37 +56,41 @@ class AdminPage extends Component {
 
     return (
       <div>
-        <h1>Admin</h1>
-        <p>The Admin Page is accessible by every signed in admin user.</p>
-
+        <h2>Users</h2>
         {loading && <div>Loading ...</div>}
-
-        <UserList users={users} />
+        <ul>
+          {users.map(user => (
+            <li key={user.uid}>
+              <span>
+                <strong>ID:</strong> {user.uid}
+              </span>
+              <span>
+                <strong>E-Mail:</strong> {user.email}
+              </span>
+              <span>
+                <strong>Username:</strong> {user.username}
+              </span>
+              <span>
+                <Link to={`${ROUTES.ADMIN}/${user.uid}`}>Details</Link>
+              </span>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
 }
 
-const UserList = ({ users }) => (
-  <ul>
-    {users.map(user => (
-      <li key={user.uid}>
-        <span>
-          <strong>ID:</strong> {user.uid}
-        </span>
-        <span>
-          <strong>E-Mail:</strong> {user.email}
-        </span>
-        <span>
-          <strong>Username:</strong> {user.username}
-        </span>
-      </li>
-    ))}
-  </ul>
+const UserItem = ({ match }) => (
+  <div>
+    <h2>User ({match.params.id})</h2>
+  </div>
 );
 
 const condition = authUser =>
   authUser && authUser.roles.includes(ROLES.ADMIN);
+
+const UserList = withFirebase(UserListBase);
 
 export default compose(
   withAuthorization(condition),
